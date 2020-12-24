770554b59ebf3b780189393da0581639b2703976
STEPin104391 <69082023+STEPin104391@users.noreply.github.com>
2020-12-24 15:42:22 +0530

    Add files via upload

---
+++ task1/Account.cpp
@@ -0,0 +1,42 @@
+#include "Account.h"
+
+ int customer_id;
+    std::string customer_name;
+    double balance;
+    int regph_no;
+
+    Account::Account():customer_id(0),customer_name(""),balance(0.0),regph_no(0)
+    {
+    }
+
+    Account::Account(int customer_id,std::string customer_name,double balance,int regph_no):customer_id(customer_id),customer_name(customer_name),balance(balance),regph_no(regph_no)
+    {
+    }
+
+    int Account::getcustomer_id() const
+    {
+        return customer_id;
+    }
+
+    std::string Account::getcustomer_name() const
+    {
+        return customer_name;
+    }
+
+    double Account::getbalance() const
+    {
+        return balance;
+    }
+
+    int Account::getph_no() const
+    {
+        return regph_no;
+    }
+
+    void Account::display() const
+    {
+        std::cout<<"\ncustomer_id:"<<customer_id
+            <<"\ncustomer_name:"<<customer_name
+            <<"\nbalance:"<<balance
+            <<"\nregistered_mobile_Number:"<<regph_no<<std::endl;
+    }
---
+++ task1/Banking.h
@@ -0,0 +1,19 @@
+#ifndef __BANKING_H
+#define __BANKING_H
+
+#include "Account.h"
+#include<list>
+
+class Banking
+{
+    std::list<Account> account;
+    public:
+    void addAccount(int,std::string,double,int);
+    void removeAccountbytid(int);
+    void displayall();
+    double averagebalance();
+    Account* findaccountbyid(int);
+    Account& maximumbalance();
+    int rangebalance(double,double);
+};
+#endif
